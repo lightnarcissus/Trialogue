3f5dd6ea28f340264985ab43b439f67ceb69b5b3
Ansh <lightnarcissus@gmail.com>
2015-09-23 13:32:23 -0400

    Imported bunch of packages from Asset Store to start setting up prototype

---
+++ Trialogue/Assets/Scripts/TreeGenerator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeGenerator : MonoBehaviour {
+
+    public GameObject[] trees;
+    public float spawnDistance=0f;
+    public float spawnRate=0.1f;
+    private int randTree = 0;
+    public GameObject oscManager;
+    public GameObject player;
+    private float randDist = 0f;
+    private float tempDist = 0f;
+    private float randZDist = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Random.value < (spawnRate * oscManager.GetComponent<oscControl>().spawnRate))
+        {
+            if (oscManager.GetComponent<oscControl>().greenTrees)
+                randTree = Random.Range(4, 7);
+            else
+                randTree = Random.Range(0, 4);
+            tempDist=oscManager.GetComponent<oscControl>().spawnDistance;
+            randDist = Random.Range(-8f, 8f);
+            randZDist = Random.Range(-8f, 8f);
+			if(trees[randTree]!=null)
+            	Instantiate(trees[randTree],player.transform.position+new Vector3(randDist*tempDist,-1.58f,randDist*tempDist),Quaternion.identity);
+        }
+	
+	}
+}
--- Trialogue/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Trialogue/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -55,6 +55,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_Jumping = false;
             m_AudioSource = GetComponent<AudioSource>();
 			m_MouseLook.Init(transform , m_Camera.transform);
+			Cursor.visible = false;
         }
 
 
