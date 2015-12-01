7c755b9d244c31a9f7d30b4dee1f917cd420bcdc
Ansh <lightnarcissus@gmail.com>
2015-12-01 07:17:13 -0500

    Small touch-up on the Critic App

--- p5JS Sketches/criticApp.js
+++ p5JS Sketches/criticApp.js
@@ -3,24 +3,43 @@ var gameplaySlider;
 var audioSlider;
 var valueSlider;
 var overallSlider;
-
+var totalScore;
+var overallScore;
 function setup() {
   // create canvas
   createCanvas(710, 400);
   textSize(15)
   noStroke();
 
+  //input
+
+  input=createInput();
+  input.position(100, 285);
+
+  button = createButton('Submit');
+  button.position(250, 285);
+   button.mousePressed(comment);
+
   // create sliders
-  graphicsSlider = createSlider(0, 255, 100);
+  graphicsSlider = createSlider(0, 10, 7);
   graphicsSlider.position(120, 120);
-  gameplaySlider = createSlider(0, 255, 0);
+  gameplaySlider = createSlider(0, 10, 7);
   gameplaySlider.position(120, 150);
-  audioSlider = createSlider(0, 255, 255);
+  audioSlider = createSlider(0, 10, 7);
   audioSlider.position(120, 180);
-  valueSlider = createSlider(0, 255, 255);
+  valueSlider = createSlider(0, 10, 7);
   valueSlider.position(120, 210);
-  overallSlider = createSlider(0, 255, 255);
+  overallSlider = createSlider(0, 10, 7);
   overallSlider.position(120, 240);
+}
+
+function comment()
+{
+  var reviewComment=input.value();
+  console.log("nice");
+  totalScore+=overallScore;
+  text("Total Score: "+totalScore,165,316)
+
 }
  
 function draw() {
@@ -28,12 +47,15 @@ function draw() {
   var gameplayScore = gameplaySlider.value();
   var audioScore = audioSlider.value();
   var valueScore = valueSlider.value();
-  var overallScore = overallSlider.value();
+  overallScore = overallSlider.value();
+
+//  totalScore+=overallScore;
 //  background(r, g, b);
   text("Graphics", 165, 135);
   text("Gameplay", 165, 165);
   text("Audio", 165, 195);
   text("Value", 165, 225);
   text("Overall", 165, 255);
+ // text("Total Score"+totalScore,165,316)
 
 }
