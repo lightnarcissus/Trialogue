1e77f34cccac165240f0c1cc38ea6aabe2e04027
Ansh <lightnarcissus@gmail.com>
2016-02-19 22:46:25 -0500

    Added Platform Manager to switch Syphon scripts on/off

---
+++ Trialogue/Assets/PlatformManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformManager : MonoBehaviour {
+
+	public string platform;
+	public GameObject playerChar;
+	public GameObject clientTexture;
+	// Use this for initialization
+	void Awake () {
+		platform = SystemInfo.operatingSystem;
+		if (platform.Contains ("Windows")) {
+			playerChar.GetComponent<Syphon> ().enabled = false;
+			playerChar.GetComponent<SyphonServerTexture> ().enabled = false;
+			clientTexture.GetComponent<SyphonClientTexture> ().enabled = false;
+		} else if (platform.Contains ("Mac")) {
+			playerChar.GetComponent<Syphon> ().enabled = true;
+			playerChar.GetComponent<SyphonServerTexture> ().enabled = true;
+			clientTexture.GetComponent<SyphonClientTexture> ().enabled = true;
+		}
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
