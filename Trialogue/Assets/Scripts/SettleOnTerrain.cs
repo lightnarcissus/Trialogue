503acfbc199f41ee896a0d263ac85f88efb818f7
Ansh <lightnarcissus@gmail.com>
2016-02-23 13:32:26 -0500

    Before playtest

--- Trialogue/Assets/Scripts/SettleOnTerrain.cs
+++ Trialogue/Assets/Scripts/SettleOnTerrain.cs
@@ -5,9 +5,10 @@ public class SettleOnTerrain : MonoBehaviour {
 
 	public LayerMask mask=11;
 	private RaycastHit hit;
+	public static int terrainStatus = 0; //0 for barren land, 1 for arena, 2 for war
 	// Use this for initialization
 	void Start () {
-
+		InvokeRepeating ("CheckStatus", 1f, 1f);
 //	`	z	if (Physics.Raycast (transform.position,Vector3.down, out hit, 100f, mask.value)) {// Raycast(ray,out hit,mask.value))
 //			if (hit.collider.gameObject.tag == "Terrain") {
 //				transform.position-=new Vector3(0f,hit.distance,0f);
@@ -24,6 +25,32 @@ public class SettleOnTerrain : MonoBehaviour {
 				transform.position-=new Vector3(0f,hit.distance,0f);
 				//Debug.Log ("hi");
 			}
+		}
+	}
+
+	void CheckStatus()
+	{
+//		if (terrainStatus == 0) {
+//			transform.GetChild (0).gameObject.SetActive (true);
+//			transform.GetChild (1).gameObject.SetActive (false);
+//			transform.GetChild (2).gameObject.SetActive (false);
+//		}
+//		else if (terrainStatus == 1) {
+//			transform.GetChild (0).gameObject.SetActive (false);
+//			transform.GetChild (1).gameObject.SetActive (false);
+//			transform.GetChild (2).gameObject.SetActive (false);
+//		} else if (terrainStatus == 2) {
+//			transform.GetChild(1).gameObject.SetActive (true);
+//			transform.GetChild(0).gameObject.SetActive (false);
+//			transform.GetChild(2).gameObject.SetActive (false);
+//		}
+//		else if (terrainStatus == 3) {
+		if(terrainStatus==3)
+		{
+		//	Debug.Log ("battlefield activated");
+			transform.FindChild("War").gameObject.SetActive (true);
+			transform.GetChild(0).gameObject.SetActive (false);
+			transform.GetChild(1).gameObject.SetActive (false);
 		}
 	}
 	
--- Trialogue/Assets/ZombieRun.cs
+++ Trialogue/Assets/ZombieRun.cs
@@ -5,11 +5,12 @@ public class ZombieRun : MonoBehaviour {
 
 	public GameObject player;
 	private NavMeshAgent agent;
-
+	private bool dead = false;
 	// Use this for initialization
 	void Start () {
-
+		player = GameObject.Find ("Player");
 		InvokeRepeating ("UpdateTarget", 1f, 5f);
+		InvokeRepeating ("CheckDeath", 1f, 2f);
 		agent = GetComponent<NavMeshAgent>();
 		agent.destination = player.transform.position; 
 	
@@ -17,16 +18,29 @@ public class ZombieRun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (agent.velocity.magnitude > 1f) {
-			gameObject.GetComponent<Animator> ().SetBool ("Running", true);
-		} else {
-			gameObject.GetComponent<Animator> ().SetBool ("Running", false);
+		if (!dead) {
+			if (agent.velocity.magnitude > 1f) {
+				gameObject.GetComponent<Animator> ().SetBool ("Running", true);
+			} else {
+				gameObject.GetComponent<Animator> ().SetBool ("Running", false);
+			}
 		}
 	}
 
 	void UpdateTarget()
 	{
-
+		if (!dead)
 		agent.destination = player.transform.position; 
+	}
+
+	void CheckDeath()
+	{
+		if (GetComponent<Animator> ().GetBool ("Death")) {
+			dead=true;
+			if(!CubeManager.bodyRemains)
+				Destroy (this.gameObject);
+			else
+				Destroy(gameObject.GetComponent<ZombieRun>());
+		}
 	}
 }
