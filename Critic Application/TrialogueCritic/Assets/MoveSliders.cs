bee275667a934af8c06820d539e81fba4d7e2443
Ansh <lightnarcissus@gmail.com>
2016-04-28 16:45:21 -0400

    After April Playtest

---
+++ Critic Application/TrialogueCritic/Assets/MoveSliders.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class MoveSliders : MonoBehaviour {
+
+    public Slider targetSlider;
+    private Vector3 startPos;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("UpdateSliders", 2f, 2f);
+        startPos = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.localPosition = new Vector3(startPos.x, transform.localPosition.y, startPos.z);
+	}
+
+    void UpdateSliders()
+    {
+
+    }
+}
